635797591910d8aaaf3b03ce26e52ae530e4f7a5
Vinson <vinsonhe2@gmail.com>
2025-01-18 22:19:13 -0800

    Complete getComputerChoice function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello world!');
+function getComputerChoice() {
+    // A range from 0-2 also works but 1-3 looks nicer
+    const choice = Math.floor(Math.random() * 3) + 1;
+
+    if (choice === 1) {
+        return "rock";
+    } else if (choice === 2) {
+        return "paper";
+    } else if (choice === 3) {
+        return "scissor";
+    } else {
+        // Just in case, not necessary but I'll include it for good measure
+        console.log('Error, must return numbers between 1-3!')
+    }
+}

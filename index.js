d241e442c9111bf0130b13fa6151512b6fd36bd9
Vinson <vinsonhe2@gmail.com>
2025-01-18 22:45:38 -0800

    Create getHumanChoice and initialize score variables

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3) + 1;
 
     if (choice === 1) {
-        return "rock";
+        return 'rock';
     } else if (choice === 2) {
-        return "paper";
+        return 'paper';
     } else if (choice === 3) {
-        return "scissor";
+        return 'scissor';
     } else {
         // Just in case, not necessary but I'll include it for good measure
         console.log('Error, must return numbers between 1-3!')
     }
 }
+
+function getHumanChoice() {
+    let input = prompt('Rock, Paper, or Scissor?');
+    let choice = input.toLowerCase();
+
+    if (choice === 'rock' || choice === 'paper' || 
+        choice === 'scissor') {
+        return choice;
+    } else {
+        console.log('That is not a valid input! Please make sure to type it correctly!');
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;

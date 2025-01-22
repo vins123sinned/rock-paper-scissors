7704025a12b89caf07f967c40aa9af5ac6869ae8
Vinson <vinsonhe2@gmail.com>
2025-01-22 11:19:42 -0800

    Add DOM methods instead of console.log
    Also added functionality to event listener instead
    of console.log

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function getComputerChoice() {
-    // A range from 0-2 also works but 1-3 looks nicer
     const choice = Math.floor(Math.random() * 3) + 1;
 
     if (choice === 1) {
@@ -9,21 +8,7 @@ function getComputerChoice() {
     } else if (choice === 3) {
         return 'scissor';
     } else {
-        // Just in case, not necessary but I'll include it for good measure
         console.log('Error, must return numbers between 1-3!')
-    }
-}
-
-function getHumanChoice() {
-    let input = prompt('Rock, Paper, or Scissor?');
-    let choice = input.toLowerCase();
-
-    if (choice === 'rock' || choice === 'paper' || 
-        choice === 'scissor') 
-    {
-        return choice;
-    } else {
-        console.log('That is not a valid input! Please make sure to type it correctly!');
     }
 }
 
@@ -33,43 +18,40 @@ function playRound(humanChoice, computerChoice) {
         humanChoice === 'scissor' && computerChoice === 'paper') 
     {
         humanScore++;
-        return console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        return displayRoundResults(`You win! ${humanChoice} beats ${computerChoice}.`);
     } else if (computerChoice === 'rock' && humanChoice === 'scissor' ||
         computerChoice === 'paper' && humanChoice === 'rock' ||
         computerChoice === 'scissor' && humanChoice === 'paper') 
     {
         computerScore++;
-        return console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-    } else if (humanChoice === 'rock' && computerChoice === 'rcok' ||
+        return displayRoundResults(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    } else if (humanChoice === 'rock' && computerChoice === 'rock' ||
         humanChoice === 'paper' && computerChoice === 'paper' ||
         humanChoice === 'scissor' && computerChoice === 'scissor'){
-        return console.log(`It's a tie! Both chose ${humanChoice}.`);
+        return displayRoundResults(`It's a tie! Both chose ${humanChoice}.`);
     } else {
-        return console.log('Error! Did you input a valid choice?');
+        return displayRoundResults('Error! Did you input a valid choice?');
     }
 }
-
-/*
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-    checkWinner();
-}
-*/
 
 function checkWinner() {
+    const announceWinner = document.createElement('h2');
+    if (humanScore > computerScore) {
+        announceWinner.textContent = 'You win!';
+    } else if (humanScore < computerScore) {
+        announceWinner.textContent = 'You lose.';
+    } else {
+        announceWinner.textContent = 'It was a tie';
+    }
+    resultDiv.appendChild(announceWinner);
+}
+
+function displayRoundResults(string) {
     console.log(`Your wins: ${humanScore}`);
     console.log(`Computer wins: ${computerScore}`);
-    console.log('...');
-    if (humanScore > computerScore) {
-        return console.log('You win!');
-    } else if (humanScore < computerScore) {
-        return console.log('You lose.');
-    } else {
-        return console.log('It was a tie')
+    resultHeader.textContent = string;
+    if (humanScore >= 5 || computerScore >= 5) {
+        checkWinner();
     }
 }
 
@@ -77,9 +59,17 @@ let humanScore = 0;
 let computerScore = 0;
 
 const choices = document.querySelectorAll('.choice');
+const resultDiv = document.querySelector('.round-result');
+const resultHeader = document.createElement('h1');
+
+resultHeader.textContent = 'Rock, paper, or scissor?';
+
+resultDiv.appendChild(resultHeader);
 
 choices.forEach(choice => {
     choice.addEventListener('click', (e) => {
-        console.log(e.target.value);
-    })
-})
+        let humanChoice = e.target.value;
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    });
+});
